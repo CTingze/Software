8b4c7dde2dd9f936f887affea543eb257bc0f904
8917-SchAcc <92835261+8917-SchAcc@users.noreply.github.com>
2022-01-11 23:06:43 +0800

    最後修改

---
+++ len.py
@@ -0,0 +1,17 @@
+import cv2
+import imutils
+import time
+
+VIDEO_IN = cv2.VideoCapture(0)
+
+while True:
+    hasFrame, frame = VIDEO_IN.read()
+    
+    img = yolo_detect(frame)
+    cv2.imshow("Frame", imutils.resize(img, width=850))
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+        
+VIDEO_IN.release()
+cv2.destroyAllWindows()
